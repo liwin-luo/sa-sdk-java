34d8f5551a8d139399fabbba4508455b61aefad0
liwin-luo <67359543+liwin-luo@users.noreply.github.com>
2020-08-10 15:17:42 +0800

    Add files via upload

--- src/main/java/org/sndo/data/sdk/java/exceptions/DebugModeException.java
+++ src/main/java/org/sndo/data/sdk/java/exceptions/DebugModeException.java
@@ -1,1 +1,20 @@
+package org.sndo.data.sdk.java.exceptions;
 
+/**
+ * Debug模式下的错误
+ */
+public class DebugModeException extends RuntimeException {
+
+  public DebugModeException(String message) {
+    super(message);
+  }
+
+  public DebugModeException(Throwable error) {
+    super(error);
+  }
+
+  public DebugModeException(String message, Throwable error) {
+    super(message, error);
+  }
+
+}
---
+++ src/main/java/org/sndo/data/sdk/java/exceptions/InvalidArgumentException.java
@@ -0,0 +1,16 @@
+package org.sndo.data.sdk.java.exceptions;
+
+/**
+ * 非法的DistinctID
+ */
+public class InvalidArgumentException extends Exception {
+
+  public InvalidArgumentException(String message) {
+    super(message);
+  }
+
+  public InvalidArgumentException(Throwable error) {
+    super(error);
+  }
+
+}
